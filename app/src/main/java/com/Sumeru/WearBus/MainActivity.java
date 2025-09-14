a1f00aee70deeaa8f765007b67c356a1981a04e5
DreamerQ2022 <dreamerq2023@gmail.com>
2025-09-14 12:14:12 +0800

    WearBus Update 1.1.1

--- app/src/main/java/com/Sumeru/WearBus/MainActivity.java
+++ app/src/main/java/com/Sumeru/WearBus/MainActivity.java
@@ -1,6 +1,7 @@
 package com.Sumeru.WearBus;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
@@ -10,10 +11,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity{
     @RequiresApi(api = Build.VERSION_CODES.O)
+    private void checkFirstRun() {
+        SharedPreferences sharedPreferences = getSharedPreferences("FirstRun", MODE_PRIVATE);
+        boolean isFirstRun = sharedPreferences.getBoolean("First", true);
+        if (isFirstRun) {
+            sharedPreferences.edit().putBoolean("First", false).apply();
+            Intent intent = new Intent(MainActivity.this,guideSplashActivity.class);
+            startActivity(intent);
+        }
+    }
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.maininterface);
+        checkFirstRun();
         Button Bus = findViewById(R.id.bus);
         Bus.setOnClickListener(v -> {
             Intent intent=new Intent(MainActivity.this,Bus.class);
@@ -25,4 +37,5 @@ public class MainActivity extends AppCompatActivity{
             startActivity(intent);
         });
     }
+
 }
